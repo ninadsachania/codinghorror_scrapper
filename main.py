6e013f33ef37ea8cae1f22fa73b3b5db79bc65e6
Ninad Sachania <ninad.sachania@gmail.com>
2019-04-12 21:13:56 +0530

    Remove useless imports

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import bs4 as bs
 import requests 
-import time
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 URL = 'https://blog.codinghorror.com'
